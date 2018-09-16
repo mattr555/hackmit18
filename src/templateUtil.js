048797b4d1670b28f4474b8a1f6563ae10ea0427
tivallancourt <tpv2106@columbia.edu>
2018-09-16 08:31:54 -0400

    date format

--- src/templateUtil.js
+++ src/templateUtil.js
@@ -18,7 +18,7 @@ function addList (style, array, left, right, subLeft, subRight, description) {
     let rightText = putTogether(array[i], right);
     outList.push(<View style={style.makeRow}><Text>{leftText}</Text><Text style={style.right}>{rightTag(rightText)}</Text></View>);
     if (subLeft || subRight) {
-    outList.push(<View style={style.makeRow}><Text style={style.subSec}>{array[i][subLeft]}</Text><Text style={style.subRight}>{array[i][subRight]}</Text></View>);
+    outList.push(<View style={style.makeRow}><Text>{array[i][subLeft]}</Text><Text style={style.subRight}>{array[i][subRight]}</Text></View>);
     }
     if (description && array[i][description]) {
       outList.push(<View style={style.description}>{formDescript(array[i], description)}</View>)
@@ -33,10 +33,16 @@ function putTogether(item, keys) {
   }
   let outList = [];
   if (typeof(keys) === "object") {
+  	var moment = require('moment');
     // array. concat the first n-1 objects using n as the delimiter
     let delim = keys[keys.length - 1];
     for (let i = 0; i < keys.length - 1; i++) {
-      outList.push(item[keys[i]]);
+      if (moment(item[keys[i]]).isValid()) {
+      	outList.push(dateConverter(item[keys[i]], "MMM YYYY"));
+      }
+      else {
+      	outList.push(item[keys[i]]);
+      }
     }
     return outList.join(delim);
   }
@@ -57,6 +63,12 @@ function formDescript(item, key) {
     }
   }
   return result;
+}
+
+function dateConverter(date, format) {
+  var moment = require('moment');
+  let convertedDate = moment(date).format(format);
+  return convertedDate
 }
 
 export function addSection (style, title, array, left, right, subLeft, subRight, description) {
