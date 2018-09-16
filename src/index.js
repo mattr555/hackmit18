3db529b378e30895531e2e7624e835e38a7eb1b6
Matthew Ramina <mattramina@gmail.com>
2018-09-16 04:22:02 -0400

    added deployment details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -14,9 +14,9 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
